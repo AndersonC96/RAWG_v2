e0ede90fc639fb28700c7f5663bdc9bda07ba465
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-07-07 18:13:32 -0300

    Update api.php

--- services/api.php
+++ services/api.php
@@ -14,3 +14,16 @@
         }
         return json_decode($response);//retorna o json da api
     };
+    function apiSearch(String $route = 'games?key=b6341c222fbd4b2182fc441c2be19751'): object{//função para pegar o json da api
+        global $baseURL;//pega a url base da api
+        global $apiKey;//pega a chave da api
+        global $url;//pega o caminho da url
+        /*echo "{$baseURL}{$route}?key={$apiKey}";//mostra o json da api
+        exit();*/
+        $response = @file_get_contents("{$baseURL}{$route}");//pega o json da api
+        //var_dump($response);//mostra o json da api
+        if(empty($response)){//se não houver resposta
+            header("Location: {$url}/pages/errors/404");//redireciona para a página de erro 404
+        }
+        return json_decode($response);//retorna o json da api
+    };
